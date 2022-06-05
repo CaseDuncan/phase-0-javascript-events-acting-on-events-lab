245d6bc47923f977697d73658eb264d98c9b5347
devdan <oluochdancun19@gmail.com>
2022-06-05 13:11:39 +0300

    ...

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function  moveDodgerRight(){
+    const dodger = document.querySelector("#dodger")
+    const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.marginRight = "-180px";
+  }
+}
+
+function  moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.marginRight = `${left +0}px`;
+  }
+}
