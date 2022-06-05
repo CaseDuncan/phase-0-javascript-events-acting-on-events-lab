12288ee7be0677e3eb6f0d40a57f0ae5154bd125
devdan <oluochdancun19@gmail.com>
2022-06-05 13:01:33 +0300

    commit tree 501482589a327372f8d858dbbdabb96b5071ae3b

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code here
+
+function  moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.marginRight = `${left + 0}px`;
+  }
+}
